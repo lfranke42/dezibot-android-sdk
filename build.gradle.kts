61ee7874fc1c83bc04665e4876b49b64e06e5a16
Luca <luca.franke21+github@gmail.com>
2026-02-15 18:08:49 +0100

    add dokka dependency, add publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    id("org.jetbrains.dokka") version "2.1.0"
+}
+
+dependencies {
+    dokkaPlugin(libs.android.documentation.plugin)
 }
