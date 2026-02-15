3bfcaa197baf99dd0e6fd98cb309af6b2ec30f36
Luca <luca.franke21+github@gmail.com>
2026-02-15 18:17:43 +0100

    remove dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
-    id("org.jetbrains.dokka") version "2.1.0"
 }
 
-dependencies {
-    dokkaPlugin(libs.android.documentation.plugin)
-}
